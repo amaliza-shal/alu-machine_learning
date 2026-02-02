f0871281e4d6a035f14d3a7c38dc91bcd7600ec0
amaliza-shal <amaliza-shal@github.com>
2026-02-02 22:40:01 +0200

    Fix 8-ridin_bareback.py implementation to return a new matrix

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 def mat_mul(mat1, mat2):
     """Performs matrix multiplication on two 2D matrices"""
-    # Check if matrices can be multiplied
-    # Number of columns in mat1 must equal number of rows in mat2
     if len(mat1[0]) != len(mat2):
         return None
-    
-    # Get dimensions
-    rows_mat1 = len(mat1)
-    cols_mat1 = len(mat1[0])
-    cols_mat2 = len(mat2[0])
-    
+
+    result = []
+    for i in range(len(mat1)):
+        new_row = []
+        for j in range(len(mat2[0])):
+            elem = 0
+            for k in range(len(mat2)):
+                elem += mat1[i][k] * mat2[k][j]
+            new_row.append(elem)
+        result.append(new_row)
+    return result
