eade8c835cd849676d66990d026098cd1a4f560d
amaliza-shal <amaliza-shal@github.com>
2026-05-27 16:07:32 +0200

    My Commits

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -1,26 +1,22 @@
 #!/usr/bin/env python3
-'''
-    A class Neuron that defines a single neuron performing
-    binary classification:
-'''
-
-
+"""Module that defines a single neuron performing binary classification"""
 import numpy as np
 
 
 class Neuron:
-    '''
-        Class Neuron
-    '''
+    """Class that defines a single neuron performing binary classification"""
 
     def __init__(self, nx):
+        """Class constructor
+
+        Args:
+            nx (int): number of input features to the neuron
         """
-        Constructor
-        """
-        if type(nx) is not int:
-            raise TypeError('nx must be an integer')
+        if not isinstance(nx, int) or isinstance(nx, bool):
+            raise TypeError("nx must be an integer")
         if nx < 1:
-            raise ValueError('nx must be a positive integer')
-        self.W = np.random.normal(size=(1, nx))
+            raise ValueError("nx must be a positive integer")
+
+        self.W = np.random.randn(1, nx)
         self.b = 0
         self.A = 0
