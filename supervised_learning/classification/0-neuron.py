50c58b7ec4aca487ca05352df8d5b1fcae41ad13
amaliza-shal <amaliza-shal@github.com>
2026-05-27 16:01:04 +0200

    My Commits

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -14,13 +14,13 @@ class Neuron:
     '''
 
     def __init__(self, nx):
-        '''
-            Constructor
-        '''
+        """
+        Constructor
+        """
         if type(nx) is not int:
             raise TypeError('nx must be an integer')
         if nx < 1:
             raise ValueError('nx must be a positive integer')
-        self.W = np.random.randn(1, nx)
+        self.W = np.random.normal(size=(1, nx))
         self.b = 0
         self.A = 0
