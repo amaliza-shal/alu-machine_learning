6bbb62e49422f53219d7cc647bea106e31a27f7d
amaliza-shal <amaliza-shal@github.com>
2026-05-30 20:02:42 +0200

    My Commits

--- supervised_learning/optimization/10-adam.py
+++ supervised_learning/optimization/10-adam.py
@@ -1,23 +1,36 @@
 #!/usr/bin/env python3
 """
-Adam Upgraded
+    Function def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
+    that creates the training operation for a neural network in
+    tensorflow using the Adam optimization algorithm:
 """
+
+
 import tensorflow as tf
 
 
 def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
     """
-    creates the training operation for a neural network in tensorflow using
-        the Adam optimization algorithm:
-    loss is the loss of the network
-    alpha is the learning rate
-    beta1 is the weight used for the first moment
-    beta2 is the weight used for the second moment
-    epsilon is a small number to avoid division by zero
-    Returns: the Adam optimization operation
+    Creates the training operation for a neural network in tensorflow
+    using the Adam optimization algorithm
+
+    Args:
+        - loss is the loss of the network
+        - alpha is the learning rate
+        - beta1 is the weight used for the first moment
+        - beta2 is the weight used for the second moment
+        - epsilon is a small number to avoid division by zero
+
+    Returns:
+        The Adam optimization operation
     """
+    # Initialize the Adam optimizer with the given parameters
     optimizer = tf.train.AdamOptimizer(learning_rate=alpha,
                                        beta1=beta1,
                                        beta2=beta2,
                                        epsilon=epsilon)
-    return optimizer.minimize(loss)
+
+    # Create the training operation by minimizing the loss
+    train_op = optimizer.minimize(loss)
+
+    return train_op
