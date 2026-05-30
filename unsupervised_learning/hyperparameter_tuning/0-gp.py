2cf2448c50bc5d07dc79971ff1c9925392903e0b
amaliza-shal <amaliza-shal@github.com>
2026-05-30 23:03:45 +0200

    my files

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -41,5 +41,3 @@ class GaussianProcess:
         sqdist = np.sum(X1**2, 1).reshape(-1, 1) + np.sum(X2**2, 1) - \
             2 * np.dot(X1, X2.T)
         return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
-
-    
