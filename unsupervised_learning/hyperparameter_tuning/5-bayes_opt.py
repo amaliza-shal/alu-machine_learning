33d172674e7d74ea84d0bbb104c5d9381aae5e7f
amaliza-shal <amaliza-shal@github.com>
2026-06-04 20:00:41 +0200

    Fix Errors

--- unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
@@ -20,8 +20,9 @@ class BayesianOptimization():
         """
         self.f = f
         self.gp = GP(X_init, Y_init, l, sigma_f)
-        xmin, xmax = bounds
-        self.X_s = np.linspace(xmin, xmax, ac_samples).reshape(-1, 1)
+        min, max = bounds
+        X_s = np.linspace(min, max, ac_samples)
+        self.X_s = (np.sort(X_s)).reshape(-1, 1)
         self.xsi = xsi
         self.minimize = minimize
 
@@ -29,43 +30,69 @@ class BayesianOptimization():
         """
         * Uses the Expected Improvement acquisition function
         Returns: X_next, EI
+        * X_next is a numpy.ndarray of shape (1,) representing the next best
+          sample point
+        * EI is a numpy.ndarray of shape (ac_samples,) containing the expected
+          improvement of each potential sample
         """
         mu, sigma = self.gp.predict(self.X_s)
 
         if self.minimize is True:
-            mu_sample_opt = np.min(self.gp.Y)
-            imp = (mu_sample_opt - mu - self.xsi)
+            optimize = np.amin(self.gp.Y)
+            imp = optimize - mu - self.xsi
+
         else:
-            mu_sample_opt = np.max(self.gp.Y)
-            imp = (mu - mu_sample_opt - self.xsi)
+            optimize = np.amax(self.gp.Y)
+            imp = mu - optimize - self.xsi
 
-        with np.errstate(divide='warn'):
-            Z = np.where(sigma > 0, imp / sigma, 0)
-            EI = imp * norm.cdf(Z) + sigma * norm.pdf(Z)
-            EI[sigma == 0.0] = 0.0
+        Z = np.zeros(sigma.shape[0])
 
-        X_next = self.X_s[np.argmax(EI)]
+        for i in range(sigma.shape[0]):
+            if sigma[i] != 0:
+                Z[i] = imp[i] / sigma[i]
+            else:
+                Z[i] = 0
 
-        return X_next, EI
+        ei = imp * norm.cdf(Z) + sigma * norm.pdf(Z)
+        ei[sigma == 0.0] = 0.0
+
+        index = np.argmax(ei)
+        best_sample = self.X_s[index]
+
+        return (best_sample, ei)
 
     def optimize(self, iterations=100):
         """
         Optimize method
         """
-        for _ in range(iterations):
-            x_next, _ = self.acquisition()
-            # If the next proposed point has already been sampled, stop early
-            if np.any(np.isclose(self.gp.X, x_next)):
+
+        X_all_s = []
+        for i in range(iterations):
+            # Find the next sampling point xt by optimizing the acquisition
+            # function over the GP: xt = argmaxx μ(x | D1:t−1)
+
+            x_opt, _ = self.acquisition()
+            # If the next proposed point is one that has already been sampled,
+            # optimization should be stopped early
+            if x_opt in X_all_s:
                 break
-            y_next = self.f(x_next)
-            self.gp.update(x_next, y_next)
+
+            y_opt = self.f(x_opt)
+
+            # Add the sample to previous samples
+            # D1: t = {D1: t−1, (xt, yt)} and update the GP
+            self.gp.update(x_opt, y_opt)
+            X_all_s.append(x_opt)
 
         if self.minimize is True:
-            idx = np.argmin(self.gp.Y)
+            index = np.argmin(self.gp.Y)
         else:
-            idx = np.argmax(self.gp.Y)
+            index = np.argmax(self.gp.Y)
 
-        x_opt = self.gp.X[idx]
-        y_opt = self.gp.Y[idx]
+        self.gp.X = self.gp.X[:-1]
+
+        x_opt = self.gp.X[index]
+        y_opt = self.gp.Y[index]
 
         return x_opt, y_opt
+    
